e77acbe3fe91ee4aad855b6ba6d8a1b71e7fbd01
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-11-17 09:06:17 +0100

    Add changes

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store'
 import App from './App';
-import './styles/normalize.scss';
-import './styles/global.scss';
-import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
